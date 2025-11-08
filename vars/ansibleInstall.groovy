99c4e00a41db1cd6c87634da3f2316f6dbb69ff1
Ramandeep Singh <reyman.raman@gmail.com>
2025-11-08 13:40:57 +0530

    Updating groovy files for Slave deployment

--- vars/ansibleInstall.groovy
+++ vars/ansibleInstall.groovy
@@ -1,17 +1,30 @@
 def call() {
-    sh """
-        echo "Checking Ansible installation..."
-        
+    sh '''
+        echo "Checking OS..."
+
+        OS=$(grep "^ID=" /etc/os-release | cut -d= -f2 | tr -d '"')
+
+        echo "Detected OS: $OS"
+        echo "Installing Ansible..."
+
         if command -v ansible >/dev/null 2>&1; then
-            echo "Ansible is already installed."
-        else
-            echo "Installing Ansible..."
-            sudo apt update -y
-            sudo apt install software-properties-common -y
-            sudo add-apt-repository --yes --update ppa:ansible/ansible
-            sudo apt install ansible -y
+            echo "Ansible already installed."
+            exit 0
         fi
+
+        if [ "$OS" = "ubuntu" ] || [ "$OS" = "debian" ]; then
+            sudo apt update -y
+            sudo apt install -y ansible
+
+        elif [ "$OS" = "amzn" ] || [ "$OS" = "rhel" ] || [ "$OS" = "centos" ]; then
+            sudo yum install -y ansible || sudo dnf install -y ansible
+
+        else
+            echo "Unsupported OS: $OS"
+            exit 1
+        fi
+
         echo "Installed Ansible Version:"
         ansible --version
-    """
+    '''
 }
